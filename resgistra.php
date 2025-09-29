8f8c249742db585fc6aa8416d423ae24ade83797
guoliveira0 <luizgustavo160206@gmail.com>
2025-09-29 12:35:26 -0300

    TEM QUE AJEITAR O CADASTRO

--- resgistra.php
+++ resgistra.php
@@ -2,34 +2,34 @@
 session_start();
 
 $nome = $_POST['registerName'];
-$email = $_POST['loginEmail'];
-$senha = $_POST['loginPassword'];
+$email = $_POST['registerEmail'];
+$senha = $_POST['registerPassword'];
 
 include 'conexao.php';
 
 if ($conectou) {
-    
-    $sql = "SELECT * FROM usuarios WHERE email = '$email' AND nome = '$nome'";
+
+    // Verifica se o usuário já existe
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
     $buscar = mysqli_query($conec, $sql);
 
     if (mysqli_num_rows($buscar) != 0) {
-
         echo "<script>alert('Usuário já cadastrado!');</script>";
-        echo "<meta http-equiv='refresh' content='0;URL=login.php'>";       
-        //$_SESSION['permissao'] = $dados['permissao'] ?? "comum"; // se não tiver no BD, assume "comum"
-        
+        echo "<meta http-equiv='refresh' content='0;URL=login.php'>";
         exit;
     } else {
-        
-        $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha');";
+        // Criptografando a senha
+        $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
 
+        $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senhaHash')";
         $inserir = mysqli_query($conec, $sql);
 
-
-        echo "<script>alert('Usuário cadastrado com sucesso!');</script>";
-
-        header("Location: login.php");
+        if ($inserir) {
+            echo "<script>alert('Usuário cadastrado com sucesso!');</script>";
+            echo "<meta http-equiv='refresh' content='0;URL=login.php'>";
+        } else {
+            echo "<script>alert('Erro ao cadastrar usuário!');</script>";
+            echo "<meta http-equiv='refresh' content='0;URL=login.php'>";
+        }
     }
 }
-
-?>
