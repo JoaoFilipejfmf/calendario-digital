3baaad66f30d5f66b119b799c4fe48d34e331ca7
guoliveira0 <luizgustavo160206@gmail.com>
2025-11-10 11:33:12 -0300

    A

--- conexao.php
+++ conexao.php
@@ -1,31 +1,5 @@
 <?php
-$host = "localhost";
-$port = 3306;
-$user = "root";
-$password = "aluno";
-$dbname = "eduCalendar";
+    require 'class/rb-mysql.php';
 
-// Conecta apenas ao servidor (sem banco)
-$conec = mysqli_connect($host, $user, $password, "", $port);
-
-if (!$conec) {
-    die("Falha na conexão com o servidor MySQL: " . mysqli_connect_error());
-}
-
-// Cria banco se não existir
-$sql = "CREATE DATABASE IF NOT EXISTS `$dbname`";
-if (mysqli_query($conec, $sql)) {
-      $criou = 1;
-} else {
-    die("Erro ao criar banco: " . mysqli_error($conec));
-}
-
-// Conecta ao banco agora já criado
-$conec = mysqli_connect($host, $user, $password, $dbname, $port);
-
-if (!$conec) {
-    die("Falha na conexão com o banco: " . mysqli_connect_error());
-} else {
-      $conectou = 1;
-}
+        R::setup('mysql:host=127.0.0.1;dbname=educalendar', 'root', 'aluno');
 ?>
